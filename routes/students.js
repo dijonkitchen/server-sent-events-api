38a69c59151bcb5e95b6185cfb8bc8abfbf592dd
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2018-03-26 18:55:58 -0700

    Feat: Add show route for students

--- routes/students.js
+++ routes/students.js
@@ -5,5 +5,6 @@ const studentController = require('../controllers/studentController')
 
 /* GET students */
 router.get('/', studentController.index);
+router.get('/:id', studentController.show);
 
 module.exports = router;
