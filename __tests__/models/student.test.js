fb53aa321d8faed8c308e7ba2e285dc57dc7bdcd
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2018-04-09 20:39:08 -0700

    Test: Check for Student handler

--- __tests__/models/student.test.js
+++ __tests__/models/student.test.js
@@ -7,6 +7,17 @@ describe('Student', () => {
     test('has all method', () => {
         expect(Student.all).toBeTruthy()
     })
+    test('handles student', () => {
+        const callback = jest.fn()
+        const student = {
+            studentId: 'qwerty'
+        }
+        const students = new Set()
+
+        Student.handleStudent(student, students, callback)
+
+        expect(callback).toHaveBeenCalledTimes(1)
+    })
     test('has find method', () => {
         expect(Student.find).toBeTruthy()
     })
