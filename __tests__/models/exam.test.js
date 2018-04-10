afbda5000a908a5a9680021a385dc92df25d54b7
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2018-04-09 22:31:14 -0700

    Test: Check for handling exams

--- __tests__/models/exam.test.js
+++ __tests__/models/exam.test.js
@@ -7,4 +7,15 @@ describe('Exam', () => {
     test('handles exams', () => {
         expect(Exam.handleScore).toBeTruthy()
     })
+    test('handles exams', () => {
+        const callback = jest.fn()
+        const score = {
+            exam: 123
+        }
+        const exams = new Set()
+
+        Exam.handleScore(score, exams, callback)
+
+        expect(callback).toHaveBeenCalledTimes(1)
+    })
 })
