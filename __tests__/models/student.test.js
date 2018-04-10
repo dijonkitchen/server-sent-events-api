4259e2f61c0a4750f66babd25e7041aa11bea50e
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2018-04-09 20:29:19 -0700

    Test: Check for Student model methods

--- __tests__/models/student.test.js
+++ __tests__/models/student.test.js
@@ -1,7 +1,13 @@
 const Student = require('../../models/student')
 
 describe('Student', () => {
-    test('works!', () => {
+    test('exists', () => {
         expect(Student).toBeTruthy()
+    })
+    test('has all method', () => {
+        expect(Student.all).toBeTruthy()
+    })
+    test('has find method', () => {
+        expect(Student.find).toBeTruthy()
     })
 })
