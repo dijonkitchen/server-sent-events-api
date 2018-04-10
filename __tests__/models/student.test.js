6a994515dd1e847ced2890d98da3f2a61597b75b
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2018-04-09 20:41:18 -0700

    Test: Handle only unique students

--- __tests__/models/student.test.js
+++ __tests__/models/student.test.js
@@ -18,6 +18,33 @@ describe('Student', () => {
 
         expect(callback).toHaveBeenCalledTimes(1)
     })
+    test('handles only unique students', () => {
+        const callback = jest.fn()
+        const student = {
+            studentId: 'qwerty'
+        }
+        const students = new Set()
+
+        Student.handleStudent(student, students, callback)
+        Student.handleStudent(student, students, callback)
+
+        expect(callback).toHaveBeenCalledTimes(1)
+    })
+    test('handles only unique students', () => {
+        const callback = jest.fn()
+        const studentData = [
+            { studentId: 'qwerty' },
+            { studentId: 'jonathan' },
+            { studentId: 'qwerty' },
+        ]
+        const students = new Set()
+
+        for (const student of studentData) {
+            Student.handleStudent(student, students, callback)
+        }
+
+        expect(callback).toHaveBeenCalledTimes(2)
+    })
     test('has find method', () => {
         expect(Student.find).toBeTruthy()
     })
