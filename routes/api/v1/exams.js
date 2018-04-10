813e3999586983590620e88d01ccfc147f2733f3
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2018-04-10 14:00:12 -0700

    Feat: Version API under v1

--- routes/api/v1/exams.js
+++ routes/api/v1/exams.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-const examController = require('../controllers/examController')
+const examController = require('../../../controllers/examController')
 
 /* GET exams */
 router.get('/', examController.index)
